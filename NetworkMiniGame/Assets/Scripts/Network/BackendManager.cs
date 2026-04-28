a418e6be1c3078524b17497ba06f008afeafb0a8
Jaehoon Jeong <wogns4168@naver.com>
2026-04-28 11:39:23 +0900

    [Add] firebase SDK

---
+++ NetworkMiniGame/Assets/Scripts/Network/BackendManager.cs
@@ -0,0 +1,73 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackendManager : MonoBehaviour
+{
+    public static BackendManager Instance { get; private set; }
+
+    private FirebaseApp app;
+    public static FirebaseApp App => Instance.app;
+
+    private FirebaseAuth auth;
+    public static FirebaseAuth Auth => Instance.auth;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => 
+        {
+            if (task.Result == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                app = FirebaseApp.DefaultInstance;
+                auth = FirebaseAuth.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("Firebase dependencies check success");
+            }
+            else
+            {
+                Debug.LogError($"Could not resolve all Firebase dependencies: {task.Result}");
+                // Firebase Unity SDK is not safe to use here.
+                app = null;
+                auth = null;
+            }
+        });
+    }
+
+    public void SignUp(string email, string password)
+    {
+        Auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            AuthResult result = task.Result;
+            Debug.Log($"Firebase user created successfully: {result.User.DisplayName} ({result.User.UserId})");
+        });
+    }
+}
